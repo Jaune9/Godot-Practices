68545461fb5c70789ba89c8cd531638925dab73b
Jaune9 <prinnydad@hotmail.fr>
2022-06-12 15:08:47 +0200

    I'M LEARNIIIIING

--- GDQuest From Zero To Code/Practice01/Scoreboard/ScoreForm.gd
+++ GDQuest From Zero To Code/Practice01/Scoreboard/ScoreForm.gd
@@ -8,7 +8,7 @@ onready var points_field := $HBoxContainer/PointField
 func _on_Ok_pressed() -> void:
 	if not name_field.text or not points_field.text:
 		return
+		
 	scoreboard.add_line(name_field.text, points_field.text)
 	scoreboard.show()
 	name_field.text = ""
-	points_field.text = ""
--- GDQuest From Zero To Code/Practice01/Scoreboard/Scoreboard.gd
+++ GDQuest From Zero To Code/Practice01/Scoreboard/Scoreboard.gd
@@ -1,11 +1,13 @@
 extends PanelContainer
 
-onready var scores_columns := $MarginContainer/VBoxContainer/ScoresColumn
 var player_scores := {
 	"Scrooge McDuck": 999999,
+	"Marie Marie Mariiiiie": 270211,
 	"Magicka De Spell": 700,
-	"Marie Marie Mariiiiie": 270211
 }
+
+onready var scores_columns := $MarginContainer/VBoxContainer/ScoresColumn
+
 
 func _ready() -> void:
 	for name in player_scores:
@@ -17,6 +19,8 @@ func add_line(player_name: String, player_points: String) -> void:
 	scores_columns.add_child(line)
 	line.set_player_name(player_name)
 	line.set_player_points(player_points)
+	
+	player_scores[player_name] = player_points
 
 
 func _on_HideButton_pressed() -> void:
