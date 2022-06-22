8034cd0f6003fc1b27d20f9355ecd53f270bf79c
Jaune9 <prinnydad@hotmail.fr>
2022-06-22 16:04:24 +0200

    first release of Dodge the Creep! woot wwoot

--- Godot Doc 2D Game/Main.gd
+++ Godot Doc 2D Game/Main.gd
@@ -6,23 +6,30 @@ var score := 0
 func _ready() -> void:
 	randomize()
 
-
 func _on_Player_hit() -> void:
-	pass # Replace with function body.
+	game_over()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_ScoreTimer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout() -> void:
 	$MobTimer.start()
@@ -36,14 +43,19 @@ func _on_MobTimer_timeout() -> void:
 	# chose a random location on Path2D
 	var mob_spawn_location = $MobPath/MobSpawnLocation
 	mob_spawn_location.offset = randi()
-	
-	# give the mob a direction perpendicular to the path direction
 	var direction = mob_spawn_location.rotation + PI / 2
-	
+
+	# give the random position to the mob
 	mob.position = mob_spawn_location.position
 	
 	# randomness !
 	direction += rand_range(-PI / 4, PI / 4)
 	mob.rotation = direction
 	
+	# speed
 	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# add the instance to the tree
+	add_child(mob)
+
