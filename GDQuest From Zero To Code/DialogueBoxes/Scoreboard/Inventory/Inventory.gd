2c454d8ba6f9b3ec0cd77b96c14fd15fdb29c699
Jaune9 <prinnydad@hotmail.fr>
2023-02-14 08:30:11 +0100

    la reprise, c'est dur

--- GDQuest From Zero To Code/DialogueBoxes/Scoreboard/Inventory/Inventory.gd
+++ GDQuest From Zero To Code/DialogueBoxes/Scoreboard/Inventory/Inventory.gd
@@ -1,12 +1,14 @@
 extends GridContainer
 
+const InventoryItem := preload("InventoryItem.tscn")
 
 func _ready() -> void:
 	for i in 25:
 		add_inventory_item()
 
 
-func add_inventory_item() -> void:
+func add_inventory_item() -> void:	
 	# Create an instance of InventoryItem.tscn.
 	# Add it as a child of this node by calling the add_child() function.
-	pass
+	var invItem := InventoryItem.instance()
+	add_child(invItem)
--- GDQuest From Zero To Code/DialogueBoxes/Scoreboard/TodoList/TodoList.gd
+++ GDQuest From Zero To Code/DialogueBoxes/Scoreboard/TodoList/TodoList.gd
@@ -1,16 +1,20 @@
 extends VBoxContainer
 
+const TodoItem := preload("res://Scoreboard/TodoList/TodoItem.tscn")
+
 onready var line_edit := $HBoxContainer/LineEdit
 
 
 func add_todo(text: String) -> void:
 	# Create an instance of the TodoItem scene, add it as a child, and set its text.
-	var todo_item
+	var todo_item := TodoItem.instance()
+	add_child(todo_item)
 	# Call the todo_item's set_text() function to change the displayed text.
+	todo_item.set_text(text)
 
 
 func _on_Button_pressed() -> void:
 	if line_edit.text:
 		# Add a todo item with the correct text.
-		pass
+		add_todo(line_edit.text)
 	line_edit.text = ""
